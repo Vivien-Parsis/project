c4f2d1c53d6b0a1e0954b49f995052b9ccbf504b
Vivien-Parsis <vivienparsis.perso@gmail.com>
2024-03-01 12:34:11 +0100

    improved nav

--- src/components/common/NavComponent.vue
+++ src/components/common/NavComponent.vue
@@ -1,20 +1,24 @@
 <template>
     <nav>
-        <router-link to="/">home</router-link>
-        <router-link to="/serie">serie</router-link>
-        <router-link to="/film">film</router-link>
+        <router-link v-if="route.meta.authReq" to="/">home</router-link>
+        <router-link v-if="route.meta.authReq" to="/serie">serie</router-link>
+        <router-link v-if="route.meta.authReq" to="/film">film</router-link>
+        <router-link v-if="route.meta.authReq" to="/favori">favori</router-link>
+        <router-link v-if="route.meta.authReq" to="/account">mon compte</router-link>
+        <router-link v-if="!route.meta.authReq" to="/signin">Se connecter</router-link>
+        <router-link v-if="!route.meta.authReq" to="/signup">Créer un compte</router-link>
         <router-link to="/contact">contact</router-link>
-        <router-link to="/favori">favori</router-link>
-        <router-link to="/account">mon compte</router-link>
     </nav>
    
 </template>
 <script setup>
-
+    import { useRoute } from "vue-router"
+    const route = useRoute()
 </script>
 <style scoped>
     nav{
         width: 100%;
+        padding:.4em 0;
         background-color: rgb(210, 210, 210);
         border-bottom: 1px solid black;
         display: flex;
