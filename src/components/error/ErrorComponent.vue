5a25ca3b230c85205fedcc1ea752c7830901f49e
Vivien-Parsis <vivienparsis.perso@gmail.com>
2024-03-05 18:15:39 +0100

    added computed in error

--- src/components/error/ErrorComponent.vue
+++ src/components/error/ErrorComponent.vue
@@ -2,9 +2,15 @@
     import { useLoadingStore } from '../../store/loading.store'
     import IconComponent from '../common/IconComponent.vue'
     import ErrorIcon from "../../assets/img/error.svg?url"
+import { computed } from 'vue';
     const { error, message } = defineProps({
         error:String,
         message:String
+    })
+    const errorMessage = computed({
+        get(){
+            return `${error ? error : ""} ${message ? "- "+message : ''}`
+        }
     })
     const loadingStore = useLoadingStore()
     loadingStore.setLoading(false)
@@ -12,7 +18,7 @@
 <template>
     <h2>
         <IconComponent :src="ErrorIcon"/>
-        {{ error ? error : "erreur" }} - {{ message ? message : "" }}
+        {{ errorMessage }}
         <IconComponent :src="ErrorIcon"/>
     </h2>
 </template>
